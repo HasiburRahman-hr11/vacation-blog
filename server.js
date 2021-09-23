9774e092b3b279e81c72b7b7dc9e161c92687158
hrhasib <mailtohrhasib@gmail.com>
2021-09-23 12:52:43 +0600

    server.js configuration

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require("path");
 
 const app = express();
 
@@ -11,9 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
 
+// For Deployment
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 // Use ROutes
 const useRoutes = require('./routes/routes');
 useRoutes(app);
+
+// For Deployment
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 8000
 mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.fvpb1.mongodb.net/vacation
