8774333a862aba3545629293ca4d90d61df0dbb9
hrhasib <mailtohrhasib@gmail.com>
2021-09-23 13:53:50 +0600

    change in server js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const path = require("path");
 
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 // For Deployment
 app.use(express.static(path.join(__dirname, "client", "build")))
@@ -19,12 +19,13 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 const useRoutes = require('./routes/routes');
 useRoutes(app);
 
+
+
+const PORT = process.env.PORT || 8000
 // For Deployment
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-
-const PORT = process.env.PORT || 8000
 mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.fvpb1.mongodb.net/vacation
 `, {
     useNewUrlParser: true,
